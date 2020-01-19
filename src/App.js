1127720e2602362625dbf66a1672be0d0a85a6d2
Raul Herranz Munoz <raul.herranz@scmspain.com>
2020-01-19 19:47:40 +0100

    Aplitting home page from App.js

--- src/App.js
+++ src/App.js
@@ -1,46 +1,23 @@
 import React, {Component} from 'react';
 import './App.css';
-import './styles/styles.css';
 
 import 'bulma/css/bulma.css'
-import {Title} from './components/Title';
-import {SearchForm} from './components/SearchForm'
-import {MoviesList} from './components/MoviesList'
 
-import {Detail} from './pages/Detail'
+import { Home } from './pages/Home'
+import { Detail } from './pages/Detail'
 
-class App extends Component {
-  state = {searched: false, results: []}
-
-  _handleResults = (results) => {
-    this.setState({results, searched: true})
-  }
-
-  _handleSearch = () => {
-      return this.state.results.length === 0
-        ? <p><span role="img" aria-label="Sad">😞</span> Sin resultados</p>
-        : <MoviesList movies={this.state.results} />
-  }
-
+class App extends Component {   
   render() {
     const url = new URL(document.location)
     const hasId = url.searchParams.has('id')
 
-    if (hasId) {
-      return <Detail id={url.searchParams.get('id')} />
-    }
+    const Page = (hasId)
+      ? <Detail id={url.searchParams.get('id')} />
+      : <Home />
 
     return (
       <div className="App">
-        <Title>Buscador de películas</Title>
-        <div className="SearchForm-wrapper">
-          <SearchForm onResults={this._handleResults} />
-        </div>
-        {
-        this.state.searched === true
-          ? this._handleSearch()
-          : null
-        }      
+        { Page }
       </div>
     )
   }
