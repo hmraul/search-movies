b8e4e352c9c5a7df71991f761be1c0764dac8211
Raul Herranz Munoz <raul.herranz@scmspain.com>
2020-01-19 18:23:45 +0100

    Add detail link page

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import './styles/styles.css';
 
 import 'bulma/css/bulma.css'
 import {Title} from './components/Title';
-import {SearchForm} from './components/SearchForm';
+import {SearchForm} from './components/SearchForm'
 import {MoviesList} from './components/MoviesList'
+
+import {Detail} from './pages/Detail'
 
 class App extends Component {
   state = {searched: false, results: []}
---
+++ src/pages/Detail.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+
+export class Detail extends Component{
+  render() {
+    return (
+      <p>Detalle de la película</p>
+    )
+  }
+}
